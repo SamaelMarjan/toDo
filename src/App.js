8634beb74faea5ac65cb959ce15f4290ffa43a9b
Samael Marjan <samaaelmarjan@gmail.com>
2022-09-11 11:36:12 +0600

    commit3

--- src/App.js
+++ src/App.js
@@ -1,44 +1,13 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Task from './components/Task';
 
 function App() {
-  const [task, setTask] = useState('');
-  const [time, setTime] = useState('');
-  const [taskList, setTaskList] = useState([]);
-
-  const addTask = () => {
-    setTaskList([...taskList, {task: task, time: time}])
-  };
 
   return (
-    <div className="App">
-      <h1>ToDo List</h1>
-      <label>Task Name:</label>
-      <input 
-        type='text'
-        id='task'
-        onChange={(e) => {
-          setTask(e.target.value)
-        }}
-      />
-      <label>Time:</label>
-      <input 
-        type='text'
-        id='time'
-        onChange={(e) => {
-          setTime(e.target.value)
-        }}
-      />
-      <button onClick={addTask}>Add</button>
-      {
-        taskList.map((task) => {
-          return (
-            <Task tasks={task.task} time={task.time} />
-          );
-        })
-      }
-    </div>
+    <>
+      <Task/>
+    </>
   );
 }
 
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -4,10 +4,10 @@ import '../App.css';
 
 const Task = ({tasks, time}) => {
   return (
-    <div className='task'>
-        <h1>TaskName: {tasks}</h1>
-        <h1>Time to complete: {time}</h1>
-    </div>
+    <>
+      <h3>todo list</h3>
+      <button>create task</button>
+    </>
   )
 }
 
